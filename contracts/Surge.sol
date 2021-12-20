a138c1a14d37ba3c956aecaaecfc5977ab6d6369
Angelica Turla <turla_angelica@yahoo.com>
2021-12-19 22:10:27 -0500

    fix: gift mint function + added some new functions and integrated ERC721Enumerable

--- contracts/Surge.sol
+++ contracts/Surge.sol
@@ -8,16 +8,17 @@ pragma solidity ^0.8.0;
 // ▄█ █▄█ █▀▄ █▄█ ██▄   ▀▄▀▄▀ █▄█ █░▀░█ ██▄ █░▀█
 import "hardhat/console.sol";
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Enumerable.sol";
 import "@openzeppelin/contracts/utils/Counters.sol";
 import "@openzeppelin/contracts/utils/Address.sol";
 import "@openzeppelin/contracts/utils/Strings.sol";
 import "@openzeppelin/contracts/utils/Context.sol";
 import "@openzeppelin/contracts/utils/math/SafeMath.sol";
 
-import "@openzeppelin/contracts/security/ReentrancyGuard.sol";
+import "@openzeppelin/contracts/secURIty/ReentrancyGuard.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 
-contract Surge is ERC721, ReentrancyGuard, Ownable {
+contract Surge is ERC721, ReentrancyGuard, Ownable, ERC721Enumerable {
     using Counters for Counters.Counter;
     using Address for address;
     using SafeMath for uint256;
@@ -36,7 +37,7 @@ contract Surge is ERC721, ReentrancyGuard, Ownable {
     uint16 public constant MAX_TOKENS = 10000;
     uint256 public constant TOKEN_PRICE = 50000000000000000; //0.05ETH
     
-    string private baseURI;
+    string private baseTokenURI;
 
     /*----------------------------------------------*/
     /*                  MODIFIERS                  */
@@ -63,8 +64,8 @@ contract Surge is ERC721, ReentrancyGuard, Ownable {
     /**
      * @dev it will not be ready to start sale upon deploy
      */
-    constructor(string memory name, string memory symbol, string memory uri) ERC721(name, symbol) {
-        setBaseURI(uri);
+    constructor(string memory name, string memory symbol, string memory baseURI) ERC721(name, symbol) {
+        setBaseURI(baseURI);
         console.log("Testing test deploy", name, symbol);
     }
 
@@ -94,6 +95,7 @@ contract Surge is ERC721, ReentrancyGuard, Ownable {
         for(uint i=0; i < totalReceivers; i++) {
             //checks if there is enough reserved token for gifting left
             require(totalGiftMints <= MAX_RESERVED_TOKENS, "No more tokens for gifting");
+            require(balanceOf(receivers[i]) + 1 <= MAX_PER_USER, "Wallet has Max number of tokens allowed");
             totalGiftMints++;
             uint256 newTokenId = _tokenIds.current() + 1;
             _safeMint(receivers[i], newTokenId);
@@ -101,13 +103,45 @@ contract Surge is ERC721, ReentrancyGuard, Ownable {
             }
         }
 
- /**************ADMIN BASE FUNCTIONS *************/ 
-    function _baseURI() internal view override(ERC721) returns(string memory) {
-        return baseURI;
+
+    //getter for tokens owened by a user
+    function getOwnerTokens(address _owner) external view returns (uint256[] memory) {
+        uint256 totalCount = balanceOf(_owner);
+        uint256[] memory tokenIds = new uint256[](totalCount);
+
+        for(uint i=0; i < totalCount; i++) {
+            tokenIds[i] = tokenOfOwnerByIndex(_owner, i);
+        }
+
+        return tokenIds;
+   }
+
+   //ERC721 Enumerable
+     function _beforeTokenTransfer(
+        address from,
+        address to,
+        uint256 tokenId
+    ) internal virtual override(ERC721, ERC721Enumerable) {
+        super._beforeTokenTransfer(from, to, tokenId);
     }
 
-    function setBaseURI(string memory uri) public onlyOwner {
-        baseURI = uri;
+    function supportsInterface(bytes4 interfaceId)
+        public
+        view
+        override(ERC721, ERC721Enumerable)
+        returns (bool)
+    {
+        return super.supportsInterface(interfaceId);
+    }
+
+
+ /**************ADMIN BASE FUNCTIONS *************/ 
+    function _baseURI() internal view override(ERC721) returns(string memory) {
+        return baseTokenURI;
+    }
+
+    function setBaseURI(string memory baseURI) public onlyOwner {
+        baseTokenURI = baseURI;
     }
 
     function startSale() external onlyOwner {
@@ -117,4 +151,13 @@ contract Surge is ERC721, ReentrancyGuard, Ownable {
     function pauseSale() external onlyOwner {
         saleIsActive = false;
     }
+
+    function getTokensMinted() public view returns(uint256) {
+        return _tokenIds.current() + 1;
+    }
+
+   function withdrawAll() public payable onlyOwner {
+    require(payable(msg.sender).send(address(this).balance));
+  }
+
 }
