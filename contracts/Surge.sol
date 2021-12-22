e6afb70051d9b805e8a0eae6a7cd2e315b8899d5
Angelica Turla <turla_angelica@yahoo.com>
2021-12-22 16:34:17 -0500

    update: reserved tokens and max tokens

--- contracts/Surge.sol
+++ contracts/Surge.sol
@@ -29,11 +29,11 @@ contract Surge is ERC721, ReentrancyGuard, Ownable {
     Counters.Counter private _tokenIds;
 
     uint8 public constant MAX_PER_USER = 8;
-    uint8 public constant MAX_RESERVED_TOKENS = 200;
+    uint8 public constant MAX_RESERVED_TOKENS = 150;
     uint8 public totalGiftMints = 0;
     bool public saleIsActive;
 
-    uint16 public constant MAX_TOKENS = 10000;
+    uint16 public constant MAX_TOKENS = 2500;
     uint256 public constant TOKEN_PRICE = 50000000000000000; //0.05ETH
     
     string private baseURI;
