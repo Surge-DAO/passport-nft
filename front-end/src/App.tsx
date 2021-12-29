eb718c24c5f367421f40dcd29bbe02c96f160e3e
Juliette Chevalier <juliettech.1393@gmail.com>
2021-12-29 13:20:46 -0500

    Removed test button from app.tsx

--- front-end/src/App.tsx
+++ front-end/src/App.tsx
@@ -1,11 +1,9 @@
 import React from 'react';
-import PrimaryButton from './components/PrimaryButton';
 import './App.scss';
 
 function App() {
   return (
     <div className="App">
-      <PrimaryButton callToAction="CLICK TO MINT" />
     </div>
 
   );
