c94a9b341d43dc62a5a11450d4ec61bd720ac4ae
Glory Agatevure <agatevureglory@gmail.com>
2022-01-05 03:29:00 +0100

    Created Social Icon component and removed empty spaces and trailing commas

--- front-end/src/App.tsx
+++ front-end/src/App.tsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import './App.scss';
 import InitialBanner from './components/InitialBanner';
+import SocialIcons from './components/SocialIcons';
 
 function App() {
   return (
     <div className="App">
       <InitialBanner />
+      <SocialIcons/>
     </div>
 
   );
--- front-end/src/components/CircleButton/index.tsx
+++ front-end/src/components/CircleButton/index.tsx
@@ -1,35 +1,34 @@
 import React from 'react';
 import { StyleSheet, css } from 'aphrodite';
+import themeVariables from '../../themeVariables.module.scss';
 
 const styles = StyleSheet.create({
-    circle: {
-      position: 'relative',
-      width: '48px',
-      height: '48px',
-      borderRadius: '50%',
-      background: 'white',
-      display: 'block'
-    },
-    img: {
-        position: 'absolute',
-        left : '50%',
-        top : '50%',
-        transform: 'translate(-50%, -50%)'
-      },
-},
-)
+  circle: {
+    position: 'relative',
+    width: '48px',
+    height: '48px',
+    borderRadius: '50%',
+    background: themeVariables.white,
+    display: 'block',
+    margin: '8px'
+  },
+  img: {
+    position: 'absolute',
+    left : '50%',
+    top : '50%',
+    transform: 'translate(-50%, -50%)'
+  }
+})
 
-interface ICircle {
+interface CircleParams {
 	img: string;
 	link: string;
 }
 
-
-export default function CircleButton(circle : ICircle) : JSX.Element {
-    return (
-        <a href={circle.link} className={`${css(styles.circle)}`}>
-            <img className={`${css(styles.img)}`} src={require(`../../images/${circle.img}`)} alt='social-icons'/>  
-        </a>
-      
-    )
+export default function CircleButton(circle: CircleParams) : JSX.Element {
+  return (
+    <a href={circle.link} className={css(styles.circle)}>
+        <img className={css(styles.img)} src={require(`../../images/${circle.img}`)} alt='social-icons'/>  
+    </a>
+  )
 }
