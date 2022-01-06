47087e3a5668c524ad911d8a1e5adb6d41561ce2
Glory Agatevure <agatevureglory@gmail.com>
2022-01-06 04:38:03 +0100

    Fixed typos and space

--- front-end/src/strings/index.js
+++ front-end/src/strings/index.js
@@ -1,13 +1,12 @@
 export const STRINGS = {
   aboutDescription:
-  `Two months ago a group of women came together and decided to take it up 
-   on themselves to balance the gender inequality within crypto.
+  `Two months ago a group of women came together and decided to take it upon themselves to balance the gender inequality within crypto.
   
   Your Surge Passport will serve as both your point of entry, as well as a Web3 
   speedboat.
   
   You’ll get access to educational platforms, indulge in Web3 organizations, 
-  receive mentorships from experts in the industry,  get whitelisted for cool 
+  receive mentorships from experts in the industry, get whitelisted for cool 
   NFT projects, as well as other perks and discounts within the crypto space.
   `,
   aboutCollection: 'About the Collection',
