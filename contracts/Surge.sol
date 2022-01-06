16d4655a03dd8ae017fcfdf4bf0da261500568d6
Courtney Jensen <courtney@prologmobile.com>
2022-01-05 19:13:50 -0800

    added base ability to setTokenRoyalty and get royaltyInfo.

--- contracts/Surge.sol
+++ contracts/Surge.sol
@@ -9,6 +9,8 @@ pragma solidity ^0.8.0;
 import "hardhat/console.sol";
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Enumerable.sol";
+
+import "@openzeppelin/contracts/interfaces/IERC2981.sol";
 import "@openzeppelin/contracts/utils/Counters.sol";
 import "@openzeppelin/contracts/utils/Address.sol";
 import "@openzeppelin/contracts/utils/Strings.sol";
@@ -18,7 +20,7 @@ import "@openzeppelin/contracts/utils/math/SafeMath.sol";
 import "@openzeppelin/contracts/security/ReentrancyGuard.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 
-contract Surge is ERC721, ReentrancyGuard, Ownable, ERC721Enumerable {
+contract Surge is ERC721, ReentrancyGuard, Ownable, ERC721Enumerable, IERC2981 {
     using Counters for Counters.Counter;
     using Address for address;
     using SafeMath for uint256;
@@ -39,9 +41,15 @@ contract Surge is ERC721, ReentrancyGuard, Ownable, ERC721Enumerable {
     uint256 public constant TOKEN_PRICE = 50000000000000000; //0.05ETH
     
     string public baseTokenURI;
+    struct Royalty {
+        address recipient;
+        uint256 salePrice;
+    }
 
+    mapping(uint256 => Royalty) internal _royalties;
     mapping(address => bool) internal _presaleApproved;
     mapping(address => bool) internal _presaleMinted;
+
 
 
     /*----------------------------------------------*/
@@ -174,11 +182,44 @@ contract Surge is ERC721, ReentrancyGuard, Ownable, ERC721Enumerable {
     function supportsInterface(bytes4 _interfaceId)
         public
         view
-        override(ERC721, ERC721Enumerable)
+        override(ERC721, ERC721Enumerable, IERC165)
         returns (bool)
     {
         return super.supportsInterface(_interfaceId);
     }
+
+    //Sets salePrice at time of blockstamp, if marketplace takes longer to return royalties they will be different
+    //and must be taken up with the marketplace
+    function _setTokenRoyalty(uint256 id, address recipient, uint256 salePrice) internal {
+        //This is so expected 'value' will be at most 10,000 which is 100%
+        require(salePrice <= 10000, "ERC2981Royalities: Too high");
+        _royalties[id] = Royalty(recipient, salePrice);
+    }
+
+    /**
+     * @dev Called with the sale price to determine how much royalty is owed and to whom.
+     * @param tokenId - the NFT asset queried for royalty information
+     * @param salePrice - the sale price of the NFT asset specified by `tokenId`
+     * @return receiver - address of who should be sent the royalty payment
+     * @return royaltyAmount - the royalty payment amount for `salePrice`
+     */
+        function royaltyInfo(uint256 tokenId, uint256 salePrice)
+        external
+        view
+        override
+        returns (address receiver, uint256 royaltyAmount)
+    {
+        require(_exists(tokenId), "Nonexistent token");
+
+        if(_royalties[tokenId].recipient != address(0)) {
+            return (_royalties[tokenId].recipient, salePrice * _royalties[tokenId].salePrice / 10000);
+        }
+        Royalty memory royalty = _royalties[tokenId];
+        if(royalty.recipient != address(0) && royalty.salePrice != 0) {
+            return (royalty.recipient, salePrice * royalty.salePrice / 10000);
+        }
+        return (address(0), 0);
+    } 
 
 
  /**************ADMIN BASE FUNCTIONS *************/ 
