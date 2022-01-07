3c0bf9eec0b777f6aa84897c6e99bdbad803ee32
Courtney Jensen <courtney@prologmobile.com>
2022-01-06 18:41:58 -0800

    added key information to .env file for what ready

--- hardhat.config.js
+++ hardhat.config.js
@@ -17,5 +17,15 @@ task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
  * @type import('hardhat/config').HardhatUserConfig
  */
 module.exports = {
-  solidity: "0.8.4",
+  solidity: "0.8.4",  
+  // networks: {
+  //   rinkeby: {
+  //     url: process.env.STAGING_KEY,
+  //     accounts: [process.env.PRIVATE_KEY],
+  //   },
+    // mainnet: {
+    //   chainId: 1,
+    //   url: process.env.PROD_KEY,
+    //   accounts: [process.env.PRIVATE_KEY],
+    // },
 };
