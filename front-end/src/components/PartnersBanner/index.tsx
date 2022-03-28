edc631cf3fcd33fa3a3c7ee0c952243f109e792d
Glory Agatevure <agatevureglory@gmail.com>
2022-03-28 01:13:03 +0100

    Added partner logos

--- front-end/src/components/PartnersBanner/index.tsx
+++ front-end/src/components/PartnersBanner/index.tsx
@@ -1,24 +1,32 @@
 import React from 'react';
 import { StyleSheet, css } from 'aphrodite';
 import themeVariables from '../../themeVariables.module.scss';
-import { STRINGS } from '../../strings';
+import {Row, Col} from 'react-bootstrap'
+import { Partners } from '../../data/PartnersItems';
 
 const styles = StyleSheet.create({
   container: {
-    height: '30vh',
+    padding: '60px',
     backgroundColor: themeVariables.lightGreyColor
   },
   logos: {
-    margin: '0 auto'
+    width: '100px',
+    height: '100px',
+    marginTop: '20px',
+    cursor: 'pointer'
   }
 });
 
 export default function PartnersBanner(): JSX.Element {
   return (
     <div className={css(styles.container)}>
-      <div className={css(styles.logos)}>
-        <p>Partner logos go here</p>
-      </div>
+        <Row>
+        {Partners.map((partner, idx) => (
+          <Col key={idx}>
+            <img className={css(styles.logos)} src={process.env.PUBLIC_URL + partner.logo} alt="partners" onClick={() => window.open(partner.link)}/>
+          </Col>
+        ))}
+      </Row>
     </div>
   );
 }
---
+++ front-end/src/data/PartnersItems.tsx
@@ -0,0 +1,33 @@
+
+export interface PartnersParams {
+  logo: string;
+  link: string;
+}
+
+export const Partners: PartnersParams[] = [
+  {
+    logo: 'assets/images/partners/consensys.png',
+    link: 'https://www.surgewomen.io/'
+  },
+  {
+    logo: 'assets/images/partners/CTC.svg',
+    link: 'https://www.surgewomen.io/'
+  },
+  {
+    logo: 'assets/images/partners/Decentology.png',
+    link: 'https://www.surgewomen.io/'
+  },
+  {
+    logo: 'assets/images/partners/eve.png',
+    link: 'https://www.surgewomen.io/'
+  },
+  {
+    logo: 'assets/images/partners/kernel.png',
+    link: 'https://www.surgewomen.io/'
+  },
+  {
+    logo: 'assets/images/partners/UnstoppableDomains.png',
+    link: 'https://www.surgewomen.io/'
+  }
+];
+
