274fe96356208fecc733219f0b3f7be7672b7708
juliettech <juliettech.1393@gmail.com>
2022-03-30 19:15:10 -0400

    Wallet connect (#59)
    * Adding connect wallet integration
    * Install git in Dockerfile to fix build
    
    Co-authored-by: Melana Hammel <melana.hammel@gmail.com>

--- front-end/src/index.tsx
+++ front-end/src/index.tsx
@@ -1,16 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Web3ReactProvider } from '@web3-react/core';
+import { Web3Provider } from "@ethersproject/providers";
 import './index.scss';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+function getLibrary(provider: any) {
+  return new Web3Provider(provider);
+}
+
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Web3ReactProvider getLibrary={getLibrary}>
+      <App />
+    </Web3ReactProvider>
   </React.StrictMode>,
   document.getElementById('root')
 );
-
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
--- front-end/src/strings/index.js
+++ front-end/src/strings/index.js
@@ -3,7 +3,7 @@ export const STRINGS = {
   aboutDescription: `6 months ago, a group of women came together and decided to work on balancing gender inequality within crypto.
 
   Today, the Surge Passport is your golden ticket to Web3. It is designed strategically to serve both as a point of entry and an accelerator to your Web3 development. And a beautiful art collection piece of course!
-  
+
   With the Passport, you get access to educational courses, scholarships, mentorships, discounts, digital tools, presale lists, IRL experiences… and this is just the beginning.
   `,
   aboutCollection: 'About the Collection',
@@ -22,20 +22,24 @@ export const STRINGS = {
   frequentlyAskedQuestions: 'Frequently Asked Questions',
   faq: 'FAQ',
   howManyPassport: 'How many passport do you want to mint?',
+  howToMint: 'How to mint?',
   learnMoreAboutSurge: 'LEARN MORE ABOUT SURGE',
+  logOut: 'Log out',
   minting: 'minting?',
+  mintingMeans: 'Minting means generating the NFT. The Surge Passport is a collection of 5,000 NFTs built through our generative art algorithm. The moment you click “mint” we generate one for you and transfer it to your address so you´re the owner and can start accessing all the perks',
   mintPassport: 'Mint Passport',
   passportPerks: 'Passport Perks',
   passportPerksDescription: 'Here are some of the amazing goodies you get when owning a Surge NFT',
   perks: 'Perks',
+  pleaseConnectWallet: 'Please connect your wallet first.',
   roadmap: 'Roadmap',
   roadmapUpdate: 'Get the latest updates on our roadmap',
   securingWomen: 'Securing women’s place in Web3',
   seePerks: 'SEE PERKS',
+  surgePassportNFT: 'Surge Passport NFT',
   surgeURL: 'https://surgewomen.io',
   team: 'Team',
+  walletNotConnected: 'Your wallet was not connected succesfully',
   whatIs: 'Wait, what is',
-  whatIsMinting: 'What is minting?',
-  mintingMeans: 'Minting means generating the NFT. The Surge Passport is a collection of 5,000 NFTs built through our generative art algorithm. The moment you click “mint” we generate one for you and transfer it to your address so you´re the owner and can start accessing all the perks',
-  howToMint: 'How to mint?',
+  whatIsMinting: 'What is minting?'
 };
