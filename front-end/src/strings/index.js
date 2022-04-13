cad67a27f062f9625fbd4b00413ebdd24d071322
Juliette Chevalier <juliettech.1393@gmail.com>
2022-04-13 16:05:12 -0400

    change cta button on the initialbanner

--- front-end/src/strings/index.js
+++ front-end/src/strings/index.js
@@ -14,7 +14,7 @@ export const STRINGS = {
 
   Surge’s ultimate goal is to tie together an enhanced ecosystem of compensated opportunities for women in Web3, by working with other mission-aligned organizations..
   `,
-  clickToMint: 'Minting April 28th, 2022',
+  clickToMint: 'Minting April 28',
   connectWallet: 'Connect Wallet',
   frequentlyAskedQuestions: 'Frequently Asked Questions',
   faq: 'FAQ',
