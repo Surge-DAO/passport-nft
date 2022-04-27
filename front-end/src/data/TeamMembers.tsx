f4137ab63b21723819413e4964008426d38cb754
Melana Hammel <42477256+melanahammel@users.noreply.github.com>
2022-04-27 12:26:49 -0500

    Update team pics for Michelle and Melana (#100)

--- front-end/src/data/TeamMembers.tsx
+++ front-end/src/data/TeamMembers.tsx
@@ -75,7 +75,13 @@ export const teamMembers: TeamMember[] = [
   {
     name: 'Michelle L.',
     role: 'Visual Design Lead',
-    image: 'assets/images/teamMembers/Michelle.png',
+    image: 'assets/images/teamMembers/michelle.png',
     link: 'https://twitter.com/PennyCandyNFT'
+  },
+  {
+    name: 'Melana H.',
+    role: 'DevOps Engineer',
+    image: 'assets/images/teamMembers/melana.jpeg',
+    link: ''
   }
 ];
